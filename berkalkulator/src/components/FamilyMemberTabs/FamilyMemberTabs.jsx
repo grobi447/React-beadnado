180b9d66189afc0ca8140a4c57f81ea71bf102e4
Gáspár Róbert <hvh9yf@inf.elte.hu>
2024-04-20 15:28:20 +0200

    AddUserToTabs

--- berkalkulator/src/components/FamilyMemberTabs/FamilyMemberTabs.jsx
+++ berkalkulator/src/components/FamilyMemberTabs/FamilyMemberTabs.jsx
@@ -1,5 +1,20 @@
-const FamilyMemberTabs = () => {
-  return <div>FamilyMemberTabs</div>;
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+
+const FamilyMemberTabs = ({ onAddUser, users }) => {
+  return (
+    <div>
+      <Tabs className="w-[400px]">
+        <TabsList>
+        {users.map((user, index) => (
+            <TabsTrigger key={index} value={index}>{user.nev}</TabsTrigger>
+          ))}
+          <Button variant='outline' onClick={onAddUser}>+</Button>
+        </TabsList>
+      </Tabs>
+      
+    </div>
+  );
 };
 
 export default FamilyMemberTabs;
--- berkalkulator/src/components/HouseholdSalaryCalculator.jsx
+++ berkalkulator/src/components/HouseholdSalaryCalculator.jsx
@@ -1,12 +1,33 @@
 import FamilyMemberTabs from "./FamilyMemberTabs/FamilyMemberTabs";
 import HouseholdSummary from "./HouseholdSummary/HouseholdSummary";
 import SalaryCalculator from "./SalaryCalculator/SalaryCalculator";
+import { useState } from "react";
 
 const HouseholdSalaryCalculator = () => {
+  const defaultState = {
+    nev: "Név",
+    ber: "0",
+    szja: false,
+    hazas: false,
+    datum: "",
+    adokedvezmeny: false,
+    csaladikedvezmeny: false,
+    eltartott: 0,
+    kedvezmenyezett: 0
+  };
+
+
+  const [users, setUsers] = useState([]);
+
+  const addUser = () => {
+    const newUser = { ...defaultState };
+    setUsers(prevUsers => [...prevUsers, newUser]);
+  };
+
   return (
     <>
       <header>
-        <FamilyMemberTabs />
+        <FamilyMemberTabs onAddUser={addUser} users={users}/>
       </header>
       <main>
         <SalaryCalculator />
